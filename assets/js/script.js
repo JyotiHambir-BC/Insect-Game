b51fac7dc3859ad033ec61aa96261f5554150add
Jyoti Hambir <qdde3t1v@students.codeinstitute.net>
2024-08-22 13:14:30 +0000

    add basic javascript in file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
 const screens = document.querySelectorAll(".screen")
+const choose_insect_btns = document.querySelectorAll(".choose-insect-btn")
+const start_btn = document.getElementById("start-btn")
+const game_container = document.getElementById("game-container") 
+const timeEl = document.getElementById("time")
+const scoreEl = document.getElementById("score")
+const message = document.getElementById("message")
 
-screens[0].classList.add("up")
+let seconds = 0;
+let score = 0;
+let selected_insect = {}
 
+start_btn.addEventListener("click", () => screens[0].classList.add("up"))
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const img = btn.querySelector("img")
+        const src = img.getAttribute("src")
+        const alt = img.getAttribute("alt")
+        selected_insect {src, alt}
+        screens[1].classList("up")
+        setTimeout(createInsect, 1000)
+        startGame();
+    })
+})
+
+function createInsect() {
+    const insect = document.createElement("div")
+    insect.classList.add("insect")
+}
