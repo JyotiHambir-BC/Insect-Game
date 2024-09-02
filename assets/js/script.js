51dfdc688202dafdf2e6da5b70af9a70eff8c1b7
Jyoti Hambir <qdde3t1v@students.codeinstitute.net>
2024-09-02 21:03:22 +0000

    add restart page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,20 @@ const game_container = document.getElementById("game-container") ;
 const timeEl = document.getElementById("time");
 const scoreEl = document.getElementById("score");
 const message = document.getElementById("message");
+const resetBtn = document.getElementById("restart-btn");
 
 let seconds = 0;
 let score = 0;
 let selected_insect = {};
 let intervalId;
+
+function resetValues()
+{
+    seconds = 0;
+    score = 0;
+    selected_insect = {};
+    intervalId;
+}
 
 start_btn.addEventListener("click", () => screens[0].classList.add("up"));
 choose_insect_btns.forEach(btn => {
@@ -24,6 +33,29 @@ choose_insect_btns.forEach(btn => {
     });
 });
 
+function homePage() {
+    resetTexts();
+    resetValues();
+    removeInsects();
+}
+
+function resetTexts()
+{
+    screens[2].classList.remove("up");
+    screens[1].classList.remove("up");
+    screens[0].classList.remove("up");
+    timeEl.innerHTML = `Time: 00:00`;
+    scoreEl.innerHTML = `Score: 0`;
+}
+
+function removeInsects()
+{    
+    const elements = game_container.getElementsByClassName("insect");
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
 function startGame() {
     intervalId = setInterval(increaseTime, 1000);
 }
@@ -32,7 +64,7 @@ function increaseTime() {
     
     let m = Math.floor(seconds / 60);
     let s = seconds % 60;
-    if(s < 5 )//|| m < 1 || (m==1 && s<1))
+    if( m < 1 || (m==1 && s<1))
         {
     m = m < 10 ? `0${m}` : m;
     s = s < 10 ? `0${s}` : s;
@@ -92,3 +124,6 @@ function getRandomLocation() {
     const y = Math.random() * (height -200) + 100;
     return {x, y};
 }
+
+// const resetBtn = document.getElementById("restart-btn");
+resetBtn.addEventListener("click", homePage);
